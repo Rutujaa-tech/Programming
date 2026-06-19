298c8c47a22a7c41c245ea2c496b43e6aea98584
Rutujaa-tech <rutujamk2004@gmail.com>
2026-06-19 22:38:14 +0530

    Add files via upload

---
+++ Python_Programming/Competitive/Assignment/Q1.py
@@ -0,0 +1,5 @@
+X = 11
+
+print(X)
+print(type(X))
+print(id(X))
---
+++ Python_Programming/Competitive/Assignment/Q3.py
@@ -0,0 +1,8 @@
+print("Enter your name:")
+name = input()
+
+print("Enter your age:")
+age = int(input())
+
+age = age + 1
+print(f"Hello {name},you will turn {age} next year")
