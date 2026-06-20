6265ba55362dd5031e51c9d177beca9efc595120
Rutujaa-tech <rutujamk2004@gmail.com>
2026-06-20 23:05:05 +0530

    Add files via upload

---
+++ C_Programming/Practice/LogicBuilding/program23.c
@@ -0,0 +1,59 @@
+//que park ticket price calculation per age
+#include<stdio.h>
+// user defined macro
+#define AGE_INVALID -1
+
+int CalculateTickitPrice(int iAge)
+{
+    //input Filter
+    if(iAge<0)
+    {
+       
+       return AGE_INVALID;
+    }
+
+    if(iAge >= 0 && iAge <= 5)
+    {
+        return 0;                   //0 is free
+    }
+
+    else if(iAge >= 6 && iAge <= 18)
+    {
+        return 500;
+    }
+    else if(iAge >= 19 && iAge <= 50)
+    {
+        return 900;
+    }
+    else
+    {
+        return 400;
+    }
+
+}
+    
+
+int main()
+{
+    int iValue = 0;
+    int iRet = 0;
+
+    printf("please enter your age to calculate ticket prices:");
+    scanf("%d",&iValue);
+
+    iRet = CalculateTickitPrice(iValue);
+    
+    if(iRet==AGE_INVALID)
+    {
+        printf("please enter positive age\n");
+    }
+
+    else
+    {
+    printf("your ticket price will be %d ruppes",iRet);
+    }
+
+    
+    return 0;
+
+}
---
+++ C_Programming/Practice/LogicBuilding/program24.c
@@ -0,0 +1,16 @@
+
+#include<stdio.h>
+
+int main()
+{
+    //sequence
+    
+    printf("Jay Ganesh...\n");
+    printf("Jay Ganesh...\n");
+    printf("Jay Ganesh...\n");
+    printf("Jay Ganesh...\n");
+    
+    
+    return 0;
+
+}
