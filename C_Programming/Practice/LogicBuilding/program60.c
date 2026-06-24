91f3d2bbc1eb31ad4d8ce784c0e9646728894ddb
Rutujaa-tech <rutujamk2004@gmail.com>
2026-06-24 12:50:03 +0530

    Add files via upload

---
+++ C_Programming/Practice/LogicBuilding/program60.c
@@ -0,0 +1,29 @@
+//Input : 5
+//output : 5 4 3 2 1 0
+
+#include<stdio.h>
+
+void Display(int iNo)
+{
+    int iCnt = 0;
+    //jump will be 2
+    for(iCnt = iNo; iCnt >= 0; iCnt--)       
+    {
+        printf("%d\t", iCnt);
+        
+    }
+
+    printf("\n");
+}
+
+int main()
+{
+    int iValue = 0;
+
+    printf("enter number:\n");
+    scanf("%d",&iValue);
+
+    Display(iValue);
+
+    return 0;
+}
