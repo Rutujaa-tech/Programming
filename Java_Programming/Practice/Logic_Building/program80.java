1628e0c5fa639469666947b3afad736a6e5fa6c6
Rutujaa-tech <rutujamk2004@gmail.com>
2026-06-29 22:59:53 +0530

    Add files via upload

---
+++ Java_Programming/Practice/Logic_Building/program80.java
@@ -0,0 +1,37 @@
+import java.util.*;
+
+class DigitX
+{
+    public void DisplayDigits(int iNo)
+    {
+        
+        int iCnt = 0;
+
+        while(iNo != 0)
+        {
+            System.out.println(iNo % 10);
+            iNo = iNo / 10;
+        }
+    }
+
+
+}
+
+class program80
+{
+
+    public static void main(String A[])
+    {
+        Scanner sobj = new Scanner(System.in);
+        DigitX dobj = new DigitX();
+
+
+        int iValue = 0;
+
+        System.out.println("Enter number:");
+        iValue = sobj.nextInt();
+
+        dobj.DisplayDigits(iValue);
+
+    }
+}
