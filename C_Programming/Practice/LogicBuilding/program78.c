093a60a84a88fb527a914b89256781a4daf78ca7
Rutujaa-tech <rutujamk2004@gmail.com>
2026-06-29 22:51:46 +0530

    Add files via upload

---
+++ C_Programming/Practice/LogicBuilding/program78.c
@@ -0,0 +1,26 @@
+//problem of digit
+#include<stdio.h>
+void DisplayDigit(int iNo)
+{
+    int iDigit = 0;
+   
+    while(iNo >0)
+    {
+        iDigit = iNo % 10;
+        printf("%d\n",iDigit);
+        iNo = iNo/10;
+    }
+}
+int main()
+{
+    int iValue = 0;
+
+    printf("enter number :\n");
+    scanf("%d",&iValue);
+
+    DisplayDigit(iValue);
+
+
+    return 0;
+
+}
