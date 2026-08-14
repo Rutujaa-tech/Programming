c42c90ffec38e69f1ad862d260d06f6d9fade86a
Rutujaa-tech <rutujamk2004@gmail.com>
2026-08-14 23:56:44 +0530

    Add files via upload

---
+++ Machine_Learning/Practice/Logic_Building/Ball_Classification6.py
@@ -0,0 +1,26 @@
+# Case Study
+from sklearn import tree
+
+def main():
+    print("Ball Classification Case Study")
+
+    Independent = [[35,1],[47,1],[90,0],[48,1],[90,0],[35,1],[92,0],[35,1],[35,1],[35,1],[96,1],[43,1],[110,0]]
+    # Testing Features= [35,1],[95,0]
+    
+    Dependent = [1,1,2,1,2,1,2,1,1,1,2,1,2]
+    # Testing labels = [1,2]
+
+    
+    model = tree.DecisionTreeClassifier()           # model object banvla
+
+    model = model.fit(Independent,Dependent)        # train zal
+
+    Result = model.predict([[35,1],[95,0]])                    # exam 
+
+    print("Predictable result of model is :",Result)
+
+if __name__ =="__main__":
+    main()
+
+    # encoding word la number krne
+    # decoding number la word krne
